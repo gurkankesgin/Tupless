3550266519b1c793a2ac9e21f63d41166f0f0fb9
gurkan <gurkan.kesgin@kocsistem.com.tr>
2020-03-20 13:44:40 +0300

    example added

--- app/src/main/java/com/tuples/android/MainActivity.kt
+++ app/src/main/java/com/tuples/android/MainActivity.kt
@@ -2,11 +2,23 @@ package com.tuples.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tuples.tupless.Quadruple
+import com.tuples.tupless.Quintuple
+import com.tuples.tupless.Septuple
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val quadruple = getQuadruple()
+        val septuple = getSepTuple()
+    }
 
+    fun getQuadruple(): Quadruple<String, Boolean, Int, Long> {
+        return Quadruple("name", true, 0, 0L)
+    }
+
+    fun getSepTuple(): Septuple<String, Boolean, Int, Long, Int, Int, Int> {
+        return Septuple("name", true, 0, 0L, 0, 0,0)
     }
 }
